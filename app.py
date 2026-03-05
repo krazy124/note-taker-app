53c14d9bc3c740b2672d2f7429b16fe0cac31526
krazy124 <william.meredith4@gmail.com>
2026-03-05 11:06:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ name = st.text_input("Enter your name")
 
 if name:
     st.write(f"Hello {name}, welcome to my app.")
+
+bigTupple = [("a","b"),("y","z"),(1,2)]
+
+st.write("the letter or number is: " + bigTupple[1][1])
